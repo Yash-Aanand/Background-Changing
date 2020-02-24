0dbbb3083d5762406622757561aa52e7b5a92a54
Yash-Aanand <61193710+Yash-Aanand@users.noreply.github.com>
2020-02-24 20:05:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,17 +59,7 @@ function draw(){
           ellipse(World.mouseX, World.mouseY, 30, 30);
           
 
-  // change the value of Red based on the mouse movement about the X axis
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
 
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
-
-  // Create an ellipse that will move around with you mouse about the X axis.
-  // Remember to fill the canvas with white paint before creating the ellipse.
-  
   
   
   
